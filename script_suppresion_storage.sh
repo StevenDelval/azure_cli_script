43005ca64ab98a9b3aaefeddf179dab8958e85a8
StevenDelval <asteven59220@free.fr>
2024-06-24 16:56:32 +0200

    feat: script to delete azure storage

---
+++ script_suppresion_storage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o allexport
+if [ -f .env ]; then
+  source .env
+else
+  echo "Erreur : fichier .env non trouvé."
+  exit 1
+fi
+set +o allexport
+
+log_with_date() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOG_FILE_STORAGE
+}
+
+az account set --subscription $SUBSCRIPTION_ID
+
+az storage account create \
+  --name  $STORAGE_NAME \
+  --resource-group $RESOURCE_GROUP_NAME
+
+
+log_with_date "Storage '$STORAGE_NAME' est en cours de suppression sous la souscription '$SUBSCRIPTION_ID'."
