b0e03fe8d6f1880c146677d67e5cdd364e7683a7
StevenDelval <asteven59220@free.fr>
2024-06-24 17:01:03 +0200

    fix: change create by delete

--- script_suppresion_storage.sh
+++ script_suppresion_storage.sh
@@ -15,9 +15,10 @@ log_with_date() {
 
 az account set --subscription $SUBSCRIPTION_ID
 
-az storage account create \
+az storage account delete \
   --name  $STORAGE_NAME \
-  --resource-group $RESOURCE_GROUP_NAME
+  --resource-group $RESOURCE_GROUP_NAME \
+  --yes
 
 
 log_with_date "Storage '$STORAGE_NAME' est en cours de suppression sous la souscription '$SUBSCRIPTION_ID'."
