bfa1852a2f6e2ed6eda614bc7cd6f5dd532ce1cd
StevenDelval <asteven59220@free.fr>
2024-06-25 09:28:19 +0200

    Add script to delete blob

---
+++ script_suppresion_blob.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o allexport
+if [ -f .env ]; then
+  source .env
+else
+  echo "Erreur : fichier .env non trouvé."
+  exit 1
+fi
+set +o allexport
+
+log_with_date() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOG_FILE_STORAGE
+}
+
+az account set --subscription $SUBSCRIPTION_ID
+
+az storage container delete --name $NOM_BLOB_CONTAINER --account-name $STORAGE_NAME
+
+log_with_date "Container '$STORAGE_NAME' dans le compte de stockage '$STORAGE_NAME' est en cours de suppression."
